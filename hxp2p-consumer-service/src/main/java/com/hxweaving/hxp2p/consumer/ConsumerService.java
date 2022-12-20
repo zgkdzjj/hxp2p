e6b20f9d9eb60cbbc27eca1192d9e169f20e2916
Jinji Zha <jinji.zha@gmail.com>
2022-12-20 12:14:21 +1100

    init commit

---
+++ hxp2p-consumer-service/src/main/java/com/hxweaving/hxp2p/consumer/ConsumerService.java
@@ -0,0 +1,12 @@
+package com.hxweaving.hxp2p.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerService {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerService.class, args);
+    }
+
+}
---
+++ hxp2p-consumer-service/src/main/java/com/hxweaving/hxp2p/consumer/controller/ConsumerController.java
@@ -0,0 +1,13 @@
+package com.hxweaving.hxp2p.consumer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsumerController {
+
+    @GetMapping
+    public String test(){
+        return "Hello World";
+    }
+}
